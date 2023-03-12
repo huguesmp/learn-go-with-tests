7ee244f4a4614ca3f5f2c7e5e74c08a4739dc286
Hugues-Étienne Moisan-Plante <huguesmp@outlook.com>
2023-03-12 09:48:05 -0400

    midpoint: arrays & slices

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Learn Go with Tests
+
+Mes exercices pour suivre le livre en ligne [disponible ici](https://quii.gitbook.io/learn-go-with-tests/).
---
+++ iteration/repeat_test.go
@@ -0,0 +1,26 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 7)
+	expected := "aaaaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func ExampleRepeat() {
+	fmt.Println(Repeat("a", 7))
+	// Output: aaaaaaa
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
